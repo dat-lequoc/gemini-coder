3e95455c705a8c0c3f1ce44a9c93fcba9c46ff94
LE Quoc Dat <quocdat.le.insacvl@gmail.com>
2025-06-10 09:05:41 +0200

    remove socket #v1

--- packages/browser/src/background/message-handler.ts
+++ packages/browser/src/background/message-handler.ts
@@ -228,10 +228,10 @@ export const setup_message_listeners = () => {
             client_id: message.client_id
           }
           // Send to the original CWC server (we'll keep this for now)
-          send_message_to_server(finished_message)
-
+          // send_message_to_server(finished_message) # no more socket
           // ALSO send to our new reliable Flask server
           try {
+            console.log('Sending result to the local Flask server:', finished_message)  
             fetch('http://localhost:5001/report_result', {
               method: 'POST',
               headers: {
